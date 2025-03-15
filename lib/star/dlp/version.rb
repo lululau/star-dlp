f1d0dd523e5203a54eab9bf40f915f7809235423
Liu Xiang <liuxiang921@gmail.com>
2025-03-15 23:40:47 +0800

    Initialize new Ruby gem 'star-dlp' with essential files including .gitignore, gemspec, README, and Rakefile. Add initial code structure, versioning, and testing setup with RSpec.

---
+++ lib/star/dlp/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Star
+  module Dlp
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/star/dlp_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Star::Dlp do
+  it "has a version number" do
+    expect(Star::Dlp::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
